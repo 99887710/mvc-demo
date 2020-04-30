ea55fec1715e1d48e35d1a7227f318ab13a130a8
jeff wu <j98728@hotmail.com>
2020-04-30 17:31:41 +0100

    tweaked static files

--- src/main/java/jw/io/mvcdemo/controller/APIController.java
+++ src/main/java/jw/io/mvcdemo/controller/APIController.java
@@ -1,20 +1,22 @@
 package jw.io.mvcdemo.controller;
 
 import jw.io.mvcdemo.model.Transaction;
+import jw.io.mvcdemo.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 @RequestMapping("/transactions")
 public class APIController {
 
+    @Autowired
+    TransactionService transactionService;
 
     @RequestMapping("/list")
     public List<Transaction> listTransactions() {
-
-        return Arrays.asList(new Transaction());
+        return transactionService.listAll();
     }
 }
